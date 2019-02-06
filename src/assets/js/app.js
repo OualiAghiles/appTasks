b62204060382518f836f6efd872b5ddfe7f594a9
ouali aghiles <ouali.aghiles@gmail.com>
2019-02-06 22:21:42 +0100

    added group panel
    init localStorage on adding new groupe panel

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,7 +3,8 @@ var UIController = (function () {
   var DOMStrings = {
     ulLinks: '.js-menu',
     btnAddPanel: '.add_group--panel',
-    inputAddPanel: '.input_panel'
+    inputAddPanel: '.input_panel',
+    inputAddPanelBtn: 'add-G-panel'
   };
   return {
     $qs: function (selector) {
@@ -16,18 +17,20 @@ var UIController = (function () {
       elem.classList.add('d-none')
       elem.parentNode.querySelector(DOMStrings.inputAddPanel).classList.remove('d-none')
     },
-    addBoardPanel:function () {
-      var html;
+    addBoardPanelLink:function (htmlElemContair, value) {
+      var html, val;
+      val = value;
       html = `<li class="nav-item">
                 <span class="nav-link active">
-                  <a href="#">Personal</a>
+                  <a href="#">${val}</a>
                   <span class="float-right">
                     <span class="badge badge-secondary mr-3">0/0</span>
                     <i class="fas fa-plus mr-2"></i>
                   </span>
                 </span>
                 <hr class="m-0" />
-              </li>`
+              </li>`;
+      htmlElemContair.insertAdjacentHTML('beforeend', html)
     },
     getDomStrings: function () {
       return(DOMStrings)
@@ -39,6 +42,13 @@ var UIController = (function () {
 })();
 
 var PanelCotroller = (function () {
+  return {
+    loadContent: function (parent, arr) {
+      arr.forEach(function (item) {
+        UIController.addBoardPanelLink(parent,item)
+      })
+    }
+  }
 
 
 })();
@@ -49,29 +59,57 @@ var TodoController = (function () {
 })();
 
 var StoreController = (function () {
+  var data;
+  if(localStorage.getItem("data") === null ){
+    data = {
+      panels: [],
+    }
+  } else {
+    data = JSON.parse(localStorage.getItem('data'))
+  }
+  return {
 
-
+    addToStor: function () {
+      window.localStorage.setItem('data', JSON.stringify(data))
+    },
+    data
+  }
 })()
 
 var AppController = (function (UICtrl,PanCtrl, TodoCtrl, StorCtrl) {
   var DOM;
-  DOM = UICtrl.getDomStrings()
-
+  var data = StorCtrl.data;
+  var addInputBtnEvent = function (data) {
+    var value, blockInput, ul;
+    blockInput = UIController.$qs(`${DOM.inputAddPanel}`);
+    value = blockInput.querySelector('input').value
+    ul = UICtrl.$qs(DOM.ulLinks)
+    UIController.addBoardPanelLink(ul, value)
+    data.panels.push(value)
+  }
+  DOM = UICtrl.getDomStrings();
   var setupEvents = function () {
 
-    var addGroup;
+    var addGroup, inputGbtn;
     addGroup =  DOM.btnAddPanel;
     UICtrl.$qs(addGroup).addEventListener('click', function () {
-      UICtrl.showInput(addGroup)
-      UIController.addBoardPanel()
+      UICtrl.showInput(addGroup);
+    });
+    inputGbtn = UICtrl.$qs(`[data-panel ="${DOM.inputAddPanelBtn}"]`);
+    console.log(inputGbtn);
+    inputGbtn.addEventListener('click', function () {
+      addInputBtnEvent(data)
+      StorCtrl.addToStor()
     })
 
 
   }
   return {
-
     init: function () {
+
+      console.log(data)
       setupEvents()
+      PanelCotroller.loadContent( UICtrl.$qs(DOM.ulLinks),data.panels)
     }
   }
 
