a943a89cec44a3384cc9f211bc03e1e2a0887b1e
ouali aghiles <ouali.aghiles@gmail.com>
2019-02-09 21:24:58 +0100

    construct the todos board wip

--- src/assets/js/plugins/todos/todosBoard.js
+++ src/assets/js/plugins/todos/todosBoard.js
@@ -25,8 +25,9 @@ var TodosPanel = (function(){
 
  // public
  return {
-  createTodoPanelTitle: function (index, value) {
-    var html;
+  createTodoPanelTitle: function (data, index, value) {
+    var html, i;
+    i= index
     html = `<div class="contentPanel active" data-index="${index}">
                 <h6 class="mt-3 d-flex">
                   <span class="display-3" style="flex: 1">${value}</span>
@@ -34,7 +35,38 @@ var TodosPanel = (function(){
                  
                 </h6>
                 <hr>
-                <div class="tasks"  data-lists="${index}">
+                
+                </div>`;
+    var container = document.querySelector('.js-container')
+    container.innerHTML = "";
+    container.insertAdjacentHTML('beforeend', html);
+    //var test = Array.prototype.slice.call()
+    data.panels.forEach(function (curr, index, arr) {
+      curr.boards.forEach(function (elem, index2, arr2) {
+        if (elem.boardName === value && elem.id === i) {
+          if(elem.description !== "") {
+            console.log (elem.description)
+          }
+          // console.log('index',index)
+          // console.log('arr',arr)
+          // console.log('value',elem.boardName)
+          // console.log('id',elem.id)
+          // console.log('description',elem.description)
+          // console.log('index2',index2)
+          // console.log('arr2',arr2)
+          console.log({curr,index, arr, elem, index2, arr2})
+        }
+
+
+      })
+    })
+  }
+ }
+})()
+/*
+*
+*
+* <div class="tasks"  data-lists="${index}">
                   <div class="form-group">
                     <label for="exampleFormControlTextarea1">Ajouter une description</label>
                     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
@@ -42,7 +74,7 @@ var TodosPanel = (function(){
                   <hr>
                   <h4>Featured</h4>
                   <div class="card mb-3">
-                    
+
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">Cras justo odio</li>
                         <li class="list-group-item">Dapibus ac facilisis in</li>
@@ -58,10 +90,4 @@ var TodosPanel = (function(){
                         <li class="list-group-item">Vestibulum at eros</li>
                       </ul>
                   </div>
-                  </div>`;
-    document.querySelector('.js-container').innerHTML = "";
-    document.querySelector('.js-container').insertAdjacentHTML('beforeend', html);
-
-  }
- }
-})()
+* */
