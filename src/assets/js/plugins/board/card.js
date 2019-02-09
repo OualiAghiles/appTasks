151a7ab980a83936244e99038e3d19da89b7e4ed
ouali aghiles <ouali.aghiles@gmail.com>
2019-02-09 21:24:04 +0100

    wip

--- src/assets/js/plugins/board/card.js
+++ src/assets/js/plugins/board/card.js
@@ -1,11 +1,11 @@
 var UICards = (function () {
   return {
-    addCard: function (parent, value, index) {
+    addCard: function (parent, value, index, description) {
       var html;
       html = `<div class="card js-list animated fadeIn delays-2" data-tasks="${index}">
             <div class="card-body">
               <h5 class="card-title">${value}</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text">${description}</p>
             </div>
             </div>`
       parent.insertAdjacentHTML('beforeend', html)
