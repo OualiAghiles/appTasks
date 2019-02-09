ae3779ce15ea8b5ffafa22a07d7d2068c9f3a340
ouali aghiles <ouali.aghiles@gmail.com>
2019-02-09 21:48:10 +0100

    extract function
    createContentTodo

--- src/assets/js/plugins/todos/todosBoard.js
+++ src/assets/js/plugins/todos/todosBoard.js
@@ -25,7 +25,40 @@ var TodosPanel = (function(){
 
  // public
  return {
-  createTodoPanelTitle: function (data, index, value) {
+   createContentTodo: function (data, value, i, container) {
+     data.panels.forEach(function (curr, index, arr) {
+       curr.boards.forEach(function (elem, index2, arr2) {
+         if (elem.boardName === value && elem.id === i) {
+           container.insertAdjacentHTML('beforeend', `<div class="tasks"  data-lists="${i}"></div>`)
+           var tasks = container.querySelector('.tasks')
+           var html;
+           if (elem.description !== "") {
+             html = `<div class="alert alert-light" role="alert">
+                          <h4>Description</h4>
+                          <p>${elem.description}</p>
+                        </div>`
+
+           } else {
+             html = `<div class="form-group">
+                    <label for="exampleFormControlTextarea1">Ajouter une description</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                  </div>`
+           }
+           tasks.insertAdjacentHTML('beforeend', html)
+           // console.log('index',index)
+           // console.log('arr',arr)
+           // console.log('value',elem.boardName)
+           // console.log('id',elem.id)
+           // console.log('description',elem.description)
+           // console.log('index2',index2)
+           // console.log('arr2',arr2)
+           console.log({curr, index, arr, elem, index2, arr2})
+         }
+
+
+       })
+     })
+   }, createTodoPanelTitle: function (data, index, value) {
     var html, i;
     i= index
     html = `<div class="contentPanel active" data-index="${index}">
@@ -41,25 +74,7 @@ var TodosPanel = (function(){
     container.innerHTML = "";
     container.insertAdjacentHTML('beforeend', html);
     //var test = Array.prototype.slice.call()
-    data.panels.forEach(function (curr, index, arr) {
-      curr.boards.forEach(function (elem, index2, arr2) {
-        if (elem.boardName === value && elem.id === i) {
-          if(elem.description !== "") {
-            console.log (elem.description)
-          }
-          // console.log('index',index)
-          // console.log('arr',arr)
-          // console.log('value',elem.boardName)
-          // console.log('id',elem.id)
-          // console.log('description',elem.description)
-          // console.log('index2',index2)
-          // console.log('arr2',arr2)
-          console.log({curr,index, arr, elem, index2, arr2})
-        }
-
-
-      })
-    })
+     TodosPanel.createContentTodo(data, value, i, container);
   }
  }
 })()
